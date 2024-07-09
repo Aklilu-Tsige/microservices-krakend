51444ddcdfbf70eaa4f8e2509c95616f5137376c
Aklilu Tsige <aklilu.tsige.w@gmail.com>
2024-07-10 00:33:27 +0200

    update dockerfile and config files

---
+++ micro4/src/config/config.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+class Config {
+    public readonly PORT: number;
+    public readonly HOSTNAME: string;
+
+    constructor() {
+        this.PORT = parseInt(process.env.PORT || '8084', 10);
+        this.HOSTNAME = process.env.HOSTNAME || 'localhost';
+    }
+}
+
+const config = new Config();
+export default config;
